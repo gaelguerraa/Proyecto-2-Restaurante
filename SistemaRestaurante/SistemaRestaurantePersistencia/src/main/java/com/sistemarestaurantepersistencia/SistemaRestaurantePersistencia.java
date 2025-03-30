e5bde482a86aaabe82a7da423788b2003abc70ad
gaelguerra <gaelguerra05@icloud.com>
2025-03-29 21:27:27 -0700

    clases

---
+++ SistemaRestaurante/SistemaRestaurantePersistencia/src/main/java/com/sistemarestaurantepersistencia/SistemaRestaurantePersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sistemarestaurantepersistencia;
+
+/**
+ *
+ * @author gael_
+ */
+public class SistemaRestaurantePersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
