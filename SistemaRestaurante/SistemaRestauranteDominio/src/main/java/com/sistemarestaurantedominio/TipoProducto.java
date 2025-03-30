5a39bea80f18f0abf23c54ce164b96c797cb7d5a
gaelguerra <gaelguerra05@icloud.com>
2025-03-30 13:15:26 -0700

    productos

---
+++ SistemaRestaurante/SistemaRestauranteDominio/src/main/java/com/sistemarestaurantedominio/TipoProducto.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package com.sistemarestaurantedominio;
+
+/**
+ *
+ * @author gael_
+ */
+public enum TipoProducto {
+    PLATILLO,
+    POSTRE,
+    BEBIDA
+}
