e6579651c4af4664575c0e27068704a19c59cb16
JorgeCuevasGastelum <jorge.cuevas252274@potros.itson.edu.mx>
2025-04-02 12:57:38 -0700

    Cambios a frame de modulo Clientes y nombres de tablas
    - se actualizo el frame de buscar clientes para poder ser reutilizable en modulo comandas
    - se agrego nombre a las tablas de las entidades que no lo tenian

--- SistemaRestaurante/SistemaRestauranteDominio/src/main/java/sistemarestaurantedominio/ClienteFrecuente.java
+++ SistemaRestaurante/SistemaRestauranteDominio/src/main/java/sistemarestaurantedominio/ClienteFrecuente.java
@@ -7,8 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "clientes_frecuentes")
 public class ClienteFrecuente implements Serializable {
 
     @Id
