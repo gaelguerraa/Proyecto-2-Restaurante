6ef44c2291a5dd59dea1bd593faebbda9f136469
gaelguerra <gaelguerra05@icloud.com>
2025-04-05 16:10:34 -0700

    otro commit merge

--- SistemaRestaurante/SistemaRestauranteNegocio/src/main/java/sistemarestaurantenegocio/implementaciones/ProductosBO.java
+++ SistemaRestaurante/SistemaRestauranteNegocio/src/main/java/sistemarestaurantenegocio/implementaciones/ProductosBO.java
@@ -124,6 +124,16 @@ public class ProductosBO implements IProductosBO {
     public List<ProductoIngredienteDTO> obtenerProductosJoin() {
         return this.productosDAO.obtenerProductosJoin();
     }
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+    
+    
+>>>>>>> Stashed changes
+=======
+    
+    
+>>>>>>> Stashed changes
     
     
 }
