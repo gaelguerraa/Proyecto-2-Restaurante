90d5d62dc1fa3c9167da69f5de0ca308161b7f75
gaelguerra <gaelguerra05@icloud.com>
2025-04-05 18:44:22 -0700

    @SuppressWarnings("unchecked")

--- SistemaRestaurante/SistemaRestaurantePresentacion/src/main/java/sistemarestaurantepresentacion/ModuloProductos/frmBuscarProductos.java
+++ SistemaRestaurante/SistemaRestaurantePresentacion/src/main/java/sistemarestaurantepresentacion/ModuloProductos/frmBuscarProductos.java
@@ -127,7 +127,7 @@ public class frmBuscarProductos extends javax.swing.JFrame {
 
 
     
-
+    @SuppressWarnings("unchecked")    
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
